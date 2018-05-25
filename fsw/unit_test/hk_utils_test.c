0fde594c261ad820e5a96803bd862cbbbc5a8a09
cnalley <cnalley@windhoverlabs.com>
2018-05-25 16:09:18 +0000

    Fixing unit tests acting on unitialized pointer issues.

--- fsw/unit_test/hk_utils_test.c
+++ fsw/unit_test/hk_utils_test.c
@@ -70,9 +70,14 @@ int32 HK_UTILS_TEST_CFE_TBL_GetAddressHook( void **TblPtr, CFE_TBL_Handle_t TblH
 void HK_ProcessIncomingHkData_Test_Nominal(void)
 {
     HK_Send_Out_Msg_t   CmdPacket;
+    char buffer[1] = {0};
 
-    hk_copy_table_entry_t    CopyTable;
-    hk_runtime_tbl_entry_t   RuntimeTable;
+    /* Initialize to zero to not act on garbage values. */
+    hk_copy_table_entry_t    CopyTable = {0};
+    hk_runtime_tbl_entry_t   RuntimeTable = {0};
+
+    /* Set a buffer for the memcpy*/
+    RuntimeTable.OutputPktAddr = (CFE_SB_MsgPtr_t) &buffer;
     
     HK_AppData.CopyTablePtr = &CopyTable;
     HK_AppData.RuntimeTablePtr = &RuntimeTable;
@@ -103,6 +108,9 @@ void HK_ProcessIncomingHkData_Test_MessageError(void)
 
     memset(&CopyTable, 0, sizeof(CopyTable));
     memset(&RuntimeTable, 0, sizeof(RuntimeTable));
+
+    /* Set if message length >= last byte acccessed to fail. */
+    CopyTable.InputOffset = sizeof(HK_Send_Out_Msg_t) + 1;
 
     HK_AppData.CopyTablePtr = &CopyTable;
     HK_AppData.RuntimeTablePtr = &RuntimeTable;
